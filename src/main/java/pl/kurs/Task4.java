8d93f207bdce81ee42058d36585742585660d5bc
empabla <magda.dyrda@gmail.com>
2022-11-11 21:11:00 +0100

    Removed initialization of minLength and maxLength in longestName and shortestName methods.

--- src/main/java/pl/kurs/Task4.java
+++ src/main/java/pl/kurs/Task4.java
@@ -3,13 +3,12 @@ package pl.kurs;
 public class Task4 {
 
     public static String shortestName(String[] names) {
-        int minLength = 0;
         String shortestName = names[0];
         for (String n : names)
             if (n.length() < shortestName.length())
                 shortestName = n;
 
-        minLength = shortestName.length();
+        int minLength = shortestName.length();
         shortestName = "";
         for (String s : names) {
             if (s.length() == minLength)
@@ -19,13 +18,12 @@ public class Task4 {
     }
 
     public static String longestName(String[] names) {
-        int maxLength = 0;
         String longestName = names[0];
         for (String n : names)
             if (n.length() > longestName.length())
                 longestName = n;
 
-        maxLength = longestName.length();
+        int maxLength = longestName.length();
         longestName = "";
         for (String s : names) {
             if (s.length() == maxLength)
